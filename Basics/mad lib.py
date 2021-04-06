f757da70881fca0a28ce4b12f18886c6d4d17b55
abdullah-hadi <71889943+abdullah-hadi@users.noreply.github.com>
2021-04-06 23:39:27 +0600

    Add files via upload

---
+++ Basics/mad lib.py
@@ -0,0 +1,7 @@
+name=input("Enter you brothers name: ")
+adjective1=input("Enter an adjective: ")
+adjective2=input("Enter another adjective: ")
+sportsname=input("enter your favourite sport: ")
+food=input("enter your favourite food: ")
+
+print(f"Do you know {name}?\nHe is not a {adjective1} boy.\nBut sometimes he is very {adjective2}.\nYou can play {sportsname} with him.\nRemember to give him {food} after that.\n")
